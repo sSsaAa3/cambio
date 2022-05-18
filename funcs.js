d0b4f566939c023eb8699ff5d3abbcae19451fba
sSsaAa3 <103163095+sSsaAa3@users.noreply.github.com>
2022-05-18 13:38:14 -0500

    Add files via upload

---
+++ funcs.js
@@ -0,0 +1,21 @@
+function cTodo1() {
+     document.getElementById("titulo").innerHTML = "PATO FELIZ";  //**Revise la parte innerALGO, esta mal.
+     document.getElementById("pix").src = "mario.gif";                //**Revise la parte "fotito", esta mal llamado el elemento imagen, cual es su ID?
+     document.getElementById("infr").innerHTML = "PATO CAMINANDO";  //**Revise la sintaxis de DOCUMENT..
+}
+function cTodo2() {
+     document.getElementById("titulo").innerHTML = "SIMI";
+     document.getElementById("pix").src = "car.jpg";
+     document.getElementById("infr").innerHTML = "LO MISMO PERO MAS BARATO";
+}
+function cTodo3() {
+     document.getElementById("titulo").innerHTML = "YO";
+     document.getElementById("pix").src = "YO.png";
+     document.getElementById("infr").innerHTML = "GABRIEL SANCHEZ VILLANUEVA";
+}
+function Oculta() {
+     document.getElementById('logito').style.display='none';    //**Revise la parte 'NINGUNO?' , esta mal.
+}
+function Muestra() {
+    document.getElementById('logito').style.display='block';     //**Revise la parte 'BLOQUEO!' , esta mal.
+}
